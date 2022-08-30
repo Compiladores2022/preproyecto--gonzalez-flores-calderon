25b0255c1d181027c2385eb11dbbc94d0e0359ff
AgustinCalderon <agustincalderon058@gmail.com>
2022-08-29 21:56:22 -0300

    Implemented functions createNode and insert

--- src/main/symbol_list/SymbolList.c
+++ src/main/symbol_list/SymbolList.c
@@ -1,2 +1,31 @@
-#include "symbollist.h"
+#include "SymbolList.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct Node * CrateNode(){
+    struct Node *newNode;
+
+    newNode = (struct Node *) malloc (sizeof(struct Node));
+    if(newNode == NULL){
+        exit(EXIT_FAILURE);
+    }
+    else{
+        return newNode;
+    }
+}
+
+void insert(SymbolList *symbolList, Symbol symbol){
+
+    struct Node *newNode;
+
+    newNode = CrateNode();
+    newNode->info = symbol;
+    newNode->next = symbolList->head->next;
+    symbolList->head = newNode;
+}
+
+
+
 
