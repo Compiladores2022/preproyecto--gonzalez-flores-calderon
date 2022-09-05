6822793984e8cac0f489c4e915ec13e45d5af2b0
yeografik <nachogonz2702@gmail.com>
2022-09-05 11:39:13 -0300

    Added a template for sintactic analysis tree tests

---
+++ src/test/sintactic_analysis_tree.c
@@ -0,0 +1,21 @@
+#include "../main/sintactic_analysis_tree/sintactic_analysis_tree.h"
+
+#include <stdio.h>
+
+//int testFunction();
+
+int main(){
+    //total tests must be manually updated when adding a new test
+    int testsPassed = 0, totalTests = 0;
+    
+    //testFunction() ? testsPassed++ : printf("test ... failed\n");
+
+    printf("tests passed: %d out of %d\n", testsPassed, totalTests);
+    return 0;
+}
+
+/*
+int testFunction() {
+    
+}
+*/
