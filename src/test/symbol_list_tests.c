535b88fa745d2dd2b08f1b43b12caa581c015f07
yeografik <nachogonz2702@gmail.com>
2022-09-05 11:18:04 -0300

    Added a new test for symbol list

--- src/test/symbol_list_tests.c
+++ src/test/symbol_list_tests.c
@@ -9,15 +9,17 @@ int addOneSymbolAndSearchIt();
 int openAndCloseLevel();
 int cannotCloseFinalLevel();
 int searchFindsSymbolInPreviousLevels();
+int searchFindsCorrectSymbol();
 
 int main(){
     //total tests must be manually updated when adding a new test
-    int testsPassed = 0, totalTests = 4;
+    int testsPassed = 0, totalTests = 5;
     
     addOneSymbolAndSearchIt() ? testsPassed++ : printf("test add one symbol and search it failed\n");
     openAndCloseLevel() ? testsPassed++ : printf("test open and close several levels failed\n");
     cannotCloseFinalLevel() ? testsPassed++ : printf("test cannot close final level failed\n");
     searchFindsSymbolInPreviousLevels() ? testsPassed++ : printf("test search finds symbol in previous levels failed\n");
+    searchFindsCorrectSymbol() ? testsPassed++ : printf("test search finds correct symbol failed\n");
     
 
     printf("tests passed: %d out of %d\n", testsPassed, totalTests);
@@ -80,6 +82,33 @@ int searchFindsSymbolInPreviousLevels() {
     openLevel(&list);
 
     return search(&list, "a") == &symbol1;
+}
+
+int searchFindsCorrectSymbol() {
+    SymbolList list;
+    list.head = NULL;
+    initialize(&list);
+
+    int a = 3;
+    Symbol symbol1 = createSymbol(INT, "a", &a);
+    int b = 89;
+    Symbol symbol2 = createSymbol(INT, "dl", &b);
+    int c = -4;
+    Symbol symbol3 = createSymbol(INT, "n", &c);
+    int d = 1;
+    Symbol symbol4 = createSymbol(BOOL, "a", &d);
+    int e = 1;
+    Symbol symbol5 = createSymbol(BOOL, "er", &e);
+
+    insert(&list, &symbol1);
+    insert(&list, &symbol2);
+    openLevel(&list);
+    insert(&list, &symbol3);
+    insert(&list, &symbol4);
+    openLevel(&list);
+    insert(&list, &symbol5);
+
+    return search(&list, "a") == &symbol4;
 }
 
 Symbol createSymbol(enum types type, char *name, void *value) {
