33b4c12343d71687f62e9e6406a575ff5896c03b
AgustinCalderon <agustincalderon058@gmail.com>
2022-09-07 11:31:48 -0300

    Create test for sintantic analysis tree

--- src/test/sintactic_analysis_tree_tests.c
+++ src/test/sintactic_analysis_tree_tests.c
@@ -2,20 +2,65 @@
 
 #include <stdio.h>
 
-//int testFunction();
+int testCreateFullTree();
+int testInvalidTree();
+int testSingleLeaf();
 
 int main(){
     //total tests must be manually updated when adding a new test
     int testsPassed = 0, totalTests = 0;
     
-    //testFunction() ? testsPassed++ : printf("test ... failed\n");
+    testCreateFullTree() ? testsPassed++ : printf("test Create Full Tree failed \n"); totalTests++;
+    testInvalidTree() ? testsPassed++ : printf("test Create Full Tree failed \n"); totalTests++;
+    testSingleLeaf() ? testsPassed++ : printf("test Create Full Tree failed \n"); totalTests++;
 
     printf("tests passed: %d out of %d\n", testsPassed, totalTests);
     return 0;
 }
 
-/*
-int testFunction() {
+int testCreateFullTree() {
     
+    int a = 0;
+    Symbol symbol1 = createSymbol(BOOL, "a", &a);
+    int b = 1;
+    Symbol symbol2 = createSymbol(BOOL, "a", &b);
+    int c = 2;
+    Symbol symbol3 = createSymbol(BOOL, "a", &c);
+    struct Node *right = createNode(&symbol1);
+    struct Node *left = createNode(&symbol2);
+    SintacticAnalysisTree *tree = createTree(&symbol3, right, left);
+
+    //printf("%d \n", *(int *)tree->head->right->info->value);
+    if(tree->head->right->info->value != symbol1.value){
+        return 0;
+    }
+    if(tree->head->left->info->value != symbol2.value){
+       return 0; 
+    }
+    if(tree->head->info->value != symbol3.value){
+        return 0;
+    }
+
+    return 1;
 }
-*/
+
+int testInvalidTree(){
+    int c = 2;
+    Symbol symbol3 = createSymbol(BOOL, "a", &c);
+    if(createTree(&symbol3, NULL, NULL) == NULL){
+        return 1;
+    }
+    return 0;
+}
+
+int testSingleLeaf(){
+    int a = 0;
+    Symbol symbol1 = createSymbol(BOOL, "a", &a);
+    struct Node *right = createNode(&symbol1);
+    int c = 2;
+    Symbol symbol3 = createSymbol(BOOL, "a", &c);
+    if(createTree(&symbol3, right, NULL) == NULL){
+        return 0;
+    }
+    return 1;
+}
