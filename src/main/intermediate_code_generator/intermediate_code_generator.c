fff71121722681db7cb00e8575ba42b980e9a6b6
yeografik <nachogonz2702@gmail.com>
2022-09-26 14:42:15 -0300

    Added instruction creation for return and assignation

--- src/main/intermediate_code_generator/intermediate_code_generator.c
+++ src/main/intermediate_code_generator/intermediate_code_generator.c
@@ -15,7 +15,6 @@ struct InstructionNode * generateIntermediateCode(struct TreeNode *tree) {
 }
 
 void generateIntermediateCode(struct TreeNode *tree, struct InstructionNode * codeList) {
-    
     generateSentenceCode(tree->left, codeList);   //left part cannot be a 'next' symbol, must be sentence or declaration
     
     if (tree->right != NULL) {  //right part is a next symbol if exists
@@ -24,16 +23,16 @@ void generateIntermediateCode(struct TreeNode *tree, struct InstructionNode * co
 }
 
 /* 
- * takes a tree and a string to write in
- * goes over the tree and adds the respective instructions to the given string
+ * takes a tree and a list of instructions to save in the generated ones
+ * returns the temporal symbol created to store the result of the current instruction
 */
 Symbol * generateSentenceCode(struct TreeNode *tree, struct InstructionNode * codeList) {
     Symbol * temp1 = NULL, temp2 = NULL;
     if (tree->left != NULL && isOperationSymbol(tree->left->info->name)) {
-        temp1 = generateSentenceCode(tree, codeList);
+        temp1 = generateSentenceCode(tree, codeList);   //generate left sentence if left is an operator
     }
     if (tree->right != NULL && isOperationSymbol(tree->right->info->name)) {
-        temp2 = generateSentenceCode(tree, codeList);
+        temp2 = generateSentenceCode(tree, codeList);   //generate right sentence if right is an operator
     }
 
     Symbol * temp3 = addCurrentInstruction(struct TreeNode *tree, struct InstructionNode * codeList, Symbol * temp1, Symbol * temp2);
@@ -46,18 +45,18 @@ int isOperationSymbol(char *symbolName) {
 }
 
 Symbol * addCurrentInstruction(struct TreeNode *tree, struct InstructionNode * codeList, Symbol * temp1, Symbol * temp2) {
-    if(temp1 == NULL){
-        temp1 = tree->left->info;    
+    if(temp1 == NULL && tree->left != NULL) {
+        temp1 = tree->left->info;
     }
-    if(temp2 == NULL){
-        temp2 = tree->right->info;       
+    if(temp2 == NULL && tree->right != NULL) {
+        temp2 = tree->right->info;
     }
     char *varNum = malloc(sizeof(char *));
     sprintf(varNum, "%d", variableNumber);
-    
+
     Symbol * temp3;
     struct InstructionNode * instruction;
-    switch (tree->info->name) {
+    switch (tree->info->name) {     //creates the instruction
         case "+":
             temp3 = createSymbol(temp1->type, "temp" + varNum, (*(int *)temp1->value + *(int *)temp2->value);
             instruction = createInstructitionNode("ADD", temp1, temp2, temp3);
@@ -77,6 +76,14 @@ Symbol * addCurrentInstruction(struct TreeNode *tree, struct InstructionNode * c
         case "||":
             temp3 = createSymbol(temp1->type, "temp" + varNum, (*(int *)temp1->value || *(int *)temp2->value);
             instruction = createInstructitionNode("OR", temp1, temp2, temp3);
+            break;
+        case "=":
+            temp3 = NULL;
+            instruction = createInstructitionNode("MOV", temp1, NULL, temp2);
+            break;
+        case "return":
+            temp3 = NULL;
+            instruction = createInstructitionNode("RET", NULL, NULL, temp2);
             break;
         default: printf("%s is not an operator\n", tree->info->name);
             exit(0);
