e95c8b485f776239621bb0b413943e3403e2c9fc
AgustinCalderon <agustincalderon058@gmail.com>
2022-09-28 15:16:49 -0300

    Solved errors on types and some bug

--- src/main/intermediate_code_generator/intermediate_code_generator.c
+++ src/main/intermediate_code_generator/intermediate_code_generator.c
@@ -1,22 +1,29 @@
+#include "instruction.h"
 #include "intermediate_code_generator.h"
-#include "stdlib.h"
-#include "utils.h"
+
+#include "../utils.h"
+#include <stdlib.h>
+#include <stdio.h>
 
 int variableNumber = 1;
 typedef enum {ADD,SUB,MUL,DIV,AND,OR} operations;
-void generateIntermediateCode2(struct TreeNode *tree, struct InstructionNode * codeList);
+
+void generateIntermediateCode2(struct TreeNode *tree, InstructionList * codeList);
+Symbol * generateSentenceCode(struct TreeNode *tree, InstructionList * codeList);
+int isOperationSymbol(char *symbolName);
+Symbol * addCurrentInstruction(struct TreeNode *tree, InstructionList * codeList, Symbol * temp1, Symbol * temp2);
 
 /*
  * takes a decorated tree and returns an equivalent three address code program as a list of instructions
 */
-struct InstructionNode * generateIntermediateCode(struct TreeNode *tree) {
-    struct InstructionNode * code;
+InstructionList * generateIntermediateCode(struct TreeNode *tree) {
+    InstructionList * code;
     generateSentenceCode(tree, code);   //left part cannot be a 'next' symbol, must be sentence or declaration
 
     return code;   
 }
 
-void generateIntermediateCode2(struct TreeNode *tree, struct InstructionNode * codeList) {
+void generateIntermediateCode2(struct TreeNode *tree, InstructionList * codeList) {
     generateSentenceCode(tree->left, codeList);   //left part cannot be a 'next' symbol, must be sentence or declaration
     
     if (tree->right != NULL) {  //right part is a next symbol if exists
@@ -28,7 +35,7 @@ void generateIntermediateCode2(struct TreeNode *tree, struct InstructionNode * c
  * takes a tree and a list of instructions to save in the generated ones
  * returns the temporal symbol created to store the result of the current instruction
 */
-Symbol * generateSentenceCode(struct TreeNode *tree, struct InstructionNode * codeList) {
+Symbol * generateSentenceCode(struct TreeNode *tree, InstructionList * codeList) {
     Symbol * temp1 = NULL; 
     Symbol * temp2 = NULL;
     if (tree->left != NULL && isOperationSymbol(tree->left->info->name)) {
@@ -47,7 +54,7 @@ int isOperationSymbol(char *symbolName) {
     return arithmeticOperation(symbolName) || booleanOperation(symbolName);
 }
 
-Symbol * addCurrentInstruction(struct TreeNode *tree, struct InstructionNode * codeList, Symbol * temp1, Symbol * temp2) {
+Symbol * addCurrentInstruction(struct TreeNode *tree, InstructionList * codeList, Symbol * temp1, Symbol * temp2) {
     if(temp1 == NULL && tree->left != NULL) {
         temp1 = tree->left->info;
     }
@@ -60,38 +67,38 @@ Symbol * addCurrentInstruction(struct TreeNode *tree, struct InstructionNode * c
     switch (stringToInt(tree->info->name)) { //creates the instruction
         case 0:
             temp3 = tree->info;
-            instruction = createInstructitionNode("ADD", temp1, temp2, temp3);
+            instruction = createInstructionNode("ADD", temp1, temp2, temp3);
             break;
         case 1:
             temp3 = tree->info;
-            instruction = createInstructitionNode("SUB", temp1, temp2, temp3);
+            instruction = createInstructionNode("SUB", temp1, temp2, temp3);
             break;
         case 2:
             temp3 = tree->info;
-            instruction = createInstructitionNode("MULT", temp1, temp2, temp3);
+            instruction = createInstructionNode("MULT", temp1, temp2, temp3);
             break;
         case 3:
             temp3 = tree->info;
-            instruction = createInstructitionNode("AND", temp1, temp2, temp3);
+            instruction = createInstructionNode("AND", temp1, temp2, temp3);
             break;
         case 4:
             temp3 = tree->info;
-            instruction = createInstructitionNode("OR", temp1, temp2, temp3);
+            instruction = createInstructionNode("OR", temp1, temp2, temp3);
             break;
         case 5:
             temp3 = NULL;
-            instruction = createInstructitionNode("MOV", temp1, NULL, temp2);
+            instruction = createInstructionNode("MOV", temp1, NULL, temp2);
             break;
         case 6:
             if (temp3 == NULL)
             temp3 = temp2;
-            instruction = createInstructitionNode("RET", NULL, NULL, temp3);
+            instruction = createInstructionNode("RET", NULL, NULL, temp3);
             break;
         default: printf("%s is not an operator\n", tree->info->name);
             exit(0);
             break;
     }
     
-    insert(codeList, instruction);
+    insertInstructionNode(codeList, instruction);
     return temp3;
 }
