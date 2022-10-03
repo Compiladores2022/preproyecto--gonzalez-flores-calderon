b2678a837eae37b38591638a89ecb4e67caad645
AgustinCalderon <agustincalderon058@gmail.com>
2022-10-03 10:32:59 -0300

    Definition new funcion and change insert in list

--- src/main/intermediate_code_generator/instruction.c
+++ src/main/intermediate_code_generator/instruction.c
@@ -6,7 +6,7 @@
 
 void initialize(InstructionList *list) {
     list->head = NULL;
-    list->last = NULL;
+    list->head = NULL;
 }
 
 struct Instruction * createInstruction(char *name, Symbol *fstOp, Symbol *sndOp, Symbol *result) {
@@ -26,6 +26,17 @@ struct Instruction * createInstruction(char *name, Symbol *fstOp, Symbol *sndOp,
     return newInstruction;
 }
 
+struct InstructionNode * createInstructionNodeEmpty(){
+    
+    struct InstructionNode *newInstructionNode;
+    newInstructionNode  = (struct InstructionNode *) malloc (sizeof(struct InstructionNode));
+    if(newInstructionNode == NULL){
+        exit(EXIT_FAILURE);
+    }
+
+    return newInstructionNode;
+}
+
 struct InstructionNode * createInstructionNode(char *name, Symbol *fstOp, Symbol *sndOp, Symbol *result) {
     struct InstructionNode *newInstructionNode;
 
@@ -41,20 +52,42 @@ struct InstructionNode * createInstructionNode(char *name, Symbol *fstOp, Symbol
     }
 }
 
-void insertInstructionNode(InstructionList *list, struct InstructionNode *newInstructionNode) {
-    if (list->head == NULL){ 
-        list->head = newInstructionNode;
+void insertInstructionNode(InstructionList *codeList, struct Instruction *Instruction) {
+    
+    struct InstructionNode *newNode = createInstructionNodeEmpty();
+    if (codeList->head == NULL){ 
+        newNode->instruction = Instruction;
+        codeList->head = newNode;    
+        newNode->next = NULL;
     }
     else{
-        list->last->next = newInstructionNode;
+        newNode->instruction = Instruction;
+        codeList->last->next = newNode;
+        newNode->next = NULL;
     }
-    list->last = newInstructionNode;
+    codeList->last = newNode;
 }
 
 void printInstructionList(InstructionList *list) {
-
-    while (list != NULL){
-        printf("name: %s" ,list->head->instruction->name);
-        list->head = list->head->next;
+    struct InstructionNode * node = list->head;
+    while (node != NULL){
+        printf("Name instruction: %s\n" ,node->instruction->name);
+        node = node->next;
     }
+}
+
+int sizeList(InstructionList * list){
+    int size = 0;
+    while (list->head != NULL){
+        list->head = list->head->next;
+        size++;
+    }
+    return size;
+}
+
+int listEmpty(InstructionList *list){
+    if(list->head == NULL)
+        return 1;
+    
+    return 0;    
 }
