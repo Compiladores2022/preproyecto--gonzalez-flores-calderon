544e8793fa0e194d6dd81278774e9ee9887633bb
AgustinCalderon <agustincalderon058@gmail.com>
2022-11-04 14:03:30 -0300

    Type Void

--- src/main/types/symbol.h
+++ src/main/types/symbol.h
@@ -1,7 +1,7 @@
 #ifndef TYPE_STRUCTS_H
 #define TYPE_STRUCTS_H
 
-typedef enum {TYPEINT, TYPEBOOL, UNDEFINED} types;
+typedef enum {TYPEINT, TYPEBOOL, UNDEFINED, TYPEVOID} types;
 
 typedef struct {
     types type;
