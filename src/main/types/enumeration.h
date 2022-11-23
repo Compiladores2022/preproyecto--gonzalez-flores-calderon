b0b8274c67aafd10e6ecde3e775aae2aa8c0f86d
AgustinCalderon <agustincalderon058@gmail.com>
2022-11-23 20:02:29 -0300

    Added flag to variables global

--- src/main/types/enumeration.h
+++ src/main/types/enumeration.h
@@ -6,4 +6,6 @@ typedef enum {TYPEINT, TYPEBOOL, UNDEFINED, TYPEVOID} types;
 
 typedef enum {METHOD, EXTERNMETHOD, CONST, VAR, TYPELESS, NEXT, RETURN, METHODCALL, IF_, IF_ELSE} identifierType;
 
+typedef enum {YES, NO}Global;
+
 #endif
